1408eab29f514185b016552048e1c04ac1023266
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:45:11 +0530

    Add files via upload

---
+++ project6/program6/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Single
+        Console.WriteLine("Enter any four digits numbers: ")
+        n = Convert.ToInt32(Console.ReadLine())
+        If n Mod 4 = 0 Then
+            Console.WriteLine(n & "is Leap year")
+        Else
+            Console.WriteLine(n & "is not Leap year")
+            Console.ReadKey()
+
+
+        End If
+
+    End Sub
+
+
+End Module
