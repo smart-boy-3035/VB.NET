d0ccde17410a7461d08fc1349e1b3c8da0bc37db
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:36:01 +0530

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Exchange()
+    End Sub
+    Function Exchange()
+        Dim a, b, t As Integer
+        Console.WriteLine("Enter any two numbers: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        b = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Value of a : " & a)
+        Console.WriteLine("Value of b : " & b)
+        t = a
+        a = b
+        b = t
+        Console.WriteLine("Exachange value of a : " & a)
+        Console.WriteLine("Exachange value of b : " & b)
+        Console.ReadKey()
+    End Function
+End Module
