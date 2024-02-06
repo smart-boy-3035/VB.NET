60a56d053b9a2e42ffedf35f6a6a1f0ecfee4133
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:44:07 +0530

    Add files via upload

---
+++ project5/program5/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim height, base As Single
+        ' Const pi As Single = 3.14
+        Dim area As Single
+        Console.Write("Enter height of a triangle: ")
+        height = convert.ToSingle(Console.ReadLine())
+        Console.Write("Enter base of a triangle: ")
+        base = convert.ToSingle(Console.ReadLine())
+
+
+        area = 0.5 * (height * base)
+
+        Console.WriteLine("Area of triangle: " & area)
+        Console.ReadKey()
+        SimpleIntrest()
+    End Sub
+    Sub SimpleIntrest()
+
+        Dim principal, time, rate As Single
+
+        Dim SimpleIntrest As Single
+        Console.Write("Enter principal Amount: ")
+        principal = convert.ToSingle(Console.ReadLine())
+        Console.Write("Enter time: ")
+        time = convert.ToSingle(Console.ReadLine())
+        Console.Write("Enter Rate of Intrest: ")
+        rate = convert.ToSingle(Console.ReadLine())
+
+        SimpleIntrest = (principal * time * rate) / 100
+
+        Console.WriteLine("Simple Intrest : " & SimpleIntrest)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
---
+++ project5/project 8/project 8/Module1.vb
@@ -0,0 +1,90 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer
+        Console.WriteLine("Enter any number in a weak: ")
+        n = Convert.ToInt16(Console.ReadLine())
+        If n = 1 Then
+            Console.WriteLine("Monday")
+        ElseIf n = 2 Then
+            Console.WriteLine("Tueday")
+        ElseIf n = 3 Then
+            Console.WriteLine("Wednesday")
+        ElseIf n = 4 Then
+            Console.WriteLine("Thrusday")
+        ElseIf n = 5 Then
+            Console.WriteLine("Friday")
+        ElseIf n = 6 Then
+            Console.WriteLine("Saturday")
+        ElseIf n = 7 Then
+            Console.WriteLine("Sunday")
+        Else
+            Console.WriteLine("Wrong Enter please try again")
+            Console.ReadKey()
+
+        End If
+        greatest()
+    End Sub
+    Sub greatest()
+        Dim a, b, c, d As Integer
+        Console.WriteLine("Enter any number : ")
+        a = Convert.ToInt16(Console.ReadLine())
+
+        Console.WriteLine("Enter any number : ")
+        b = Convert.ToInt16(Console.ReadLine())
+
+        Console.WriteLine("Enter any number : ")
+        c = Convert.ToInt16(Console.ReadLine())
+
+        Console.WriteLine("Enter any number : ")
+        d = Convert.ToInt16(Console.ReadLine())
+
+        If a > b And a > c And a > d Then
+            Console.WriteLine(a & " is greatest")
+
+        ElseIf b > a And b > c And b > d Then
+            Console.WriteLine(b & " is greatest")
+
+        ElseIf c > a And c > b And c > d Then
+            Console.WriteLine(c & " is greatest")
+        Else
+            Console.WriteLine(d & " is greatest")
+            Console.ReadKey()
+
+
+        End If
+        smallest()
+    End Sub
+    Sub smallest()
+        Dim a, b, c, d As Integer
+        Console.WriteLine("Enter any number : ")
+        a = Convert.ToInt16(Console.ReadLine())
+
+        Console.WriteLine("Enter any number : ")
+        b = Convert.ToInt16(Console.ReadLine())
+
+        Console.WriteLine("Enter any number : ")
+        c = Convert.ToInt16(Console.ReadLine())
+
+        Console.WriteLine("Enter any number : ")
+        d = Convert.ToInt16(Console.ReadLine())
+
+        If a < b And a < c And a < d Then
+            Console.WriteLine(a & " is smallest")
+
+        ElseIf b < a And b < c And b < d Then
+            Console.WriteLine(b & " is smallest")
+
+        ElseIf c < a And c < b And c < d Then
+            Console.WriteLine(c & " is smallest")
+        Else
+            Console.WriteLine(d & " is smallest")
+            Console.ReadKey()
+
+
+        End If
+
+
+
+    End Sub
+End Module
