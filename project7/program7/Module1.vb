916b9d6e4dc3693387d6ee753ef4cbde27333966
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:46:14 +0530

    Add files via upload

---
+++ project7/program7/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer
+        Console.WriteLine("Enter any  number: ")
+        x = Convert.ToInt32(Console.ReadLine())
+        If x Mod 2 = 0 Then
+            Console.WriteLine(x & " is even number")
+        Else
+            Console.WriteLine(x & " is odd number")
+            Console.ReadKey()
+        End If
+        check()
+    End Sub
+    Sub check()
+        Dim x As Integer
+        Console.WriteLine("Enter any  number: ")
+        x = Convert.ToInt32(Console.ReadLine())
+        If x > 0 Then
+            Console.WriteLine(x & " is positive number")
+        Else
+            Console.WriteLine(x & " is negetive number")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
