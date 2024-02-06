0c1c30947bed48e99146fd3ddb14b09a32f64c2b
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:33:55 +0530

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num As Integer
+        Console.WriteLine("Enter a numbers: ")
+        num = Convert.ToInt32(Console.ReadLine())
+        prime(num)
+    End Sub
+    Function prime(ByVal n As Integer) As Integer
+        Dim c As Integer = 0
+        Dim i As Short
+        Console.WriteLine("Value of n: " & n)
+        For i = 1 To n Step 1
+            If n Mod i = 0 Then
+                c = c + 1
+            End If
+
+        Next
+        If c = 2 Then
+            Console.WriteLine("Prime Number")
+        Else
+            Console.WriteLine(" Not Prime Number")
+        End If
+
+        ' Return (c)
+        Console.ReadKey()
+       
+    End Function
+End Module
