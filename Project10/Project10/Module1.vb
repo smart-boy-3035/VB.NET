325049ec36d1f69575d991b1015206bb132bb7aa
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:47:06 +0530

    Add files via upload

---
+++ Project10/Project10/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer
+        Dim i As Short
+        Console.Write("Enter a number: ")
+        n = Console.ReadLine()
+        For i = 1 To 10 Step 1
+            Console.WriteLine(n * i)
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
