a6cc07c33f87df1d914fefa5dc0d7754635df148
smart-boy-3035 <147682581+smart-boy-3035@users.noreply.github.com>
2024-02-06 14:41:44 +0530

    Add files via upload

---
+++ project3/program3/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Module Module1
+
+    Sub Main()
+        Dim radious As Single 'floating point data
+        Const pi As Single = 3.14 'constant decalaration
+        Dim area As Single
+        Console.WriteLine("Enter radious of circle : ")
+        radious = Convert.ToSingle(Console.ReadLine())
+        area = pi * (radious ^ 2)
+        Console.WriteLine("Radious of circle: " & radious)
+        Console.WriteLine("Area of circle: " & area)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
